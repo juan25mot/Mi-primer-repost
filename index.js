d89cfb81f803e0eac4c50bac1ae713d9b9c0f10d
unknown <admin@BGAI201-024.bga.upb>
2025-09-18 19:45:11 -0500

    mensaje modificado

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hola Mundo'));
+app.get('/', (req, res) => res.send('Hola Mundo juan'));
 app.listen(3000);
